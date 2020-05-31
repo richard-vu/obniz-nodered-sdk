aebd8a2833ad5cbeb2ec8d4b5dd34875bbd31833
Katacoda Scenario <scenario@katacoda.com>
2020-05-31 16:50:01 +0000

    Add node status, name and hanling to close

--- obniz.js
+++ obniz.js
@@ -1,4 +1,3 @@
-
 const Obniz = require("obniz");
 
 module.exports = function(RED) {
@@ -8,13 +7,21 @@ module.exports = function(RED) {
     this.obnizId = n.obnizId;
     var node = this;
 
+    node.status({ fill: 'green', shape: 'ring', text: 'connecting...' });
     var connected = false;
     var obniz = new Obniz(this.obnizId);
+
     obniz.onconnect = function(){
       connected = true;
+      node.status({ fill: 'green', shape: 'ring', text: 'ping to obniz...' });
+      obniz.pingWait().then(function () {
+        node.status({ fill: 'green', shape: 'dot', text: 'connected' });
+      });
     };
+
     obniz.onclose = function(){
       connected = false;
+      node.status({ fill: 'red', shape: 'ring', text: 'disconnected' });
     };
 
     this.on('input', function(msg) {
@@ -22,6 +29,10 @@ module.exports = function(RED) {
       if(obniz && connected){
         obniz.send(payload);
       }
+    });
+
+    node.on('close', function () {
+      obniz.close();
     });
   }
   RED.nodes.registerType("obniz",obniz);
